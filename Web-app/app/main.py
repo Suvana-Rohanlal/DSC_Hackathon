c0aee94604a986327eca0a5c259f2a3d347e1040
rcedras0010@gmail.com <ronaldocedras@gmail.com>
2020-07-30 22:01:49 +0200

    web app edit

--- Web-app/app/main.py
+++ Web-app/app/main.py
@@ -1,6 +1,5 @@
-from app import app
 from flask import Flask, render_template
-
+app = Flask(__name__)
 
 @app.route('/about')
 def about():
@@ -26,3 +25,6 @@ def register():
 @app.route('/search')
 def search():
     return render_template('search.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
